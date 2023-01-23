aac89456460859dd7ede2a1b72e9c84ba21289bd
Jon Dayley <jonathandayley@gmail.com>
2023-01-23 13:57:37 -0800

    Initial commit

---
+++ Swift Fixer/Swift_FixerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_FixerApp.swift
+//  Swift Fixer
+//
+//  Created by Jonathan Dayley on 1/23/23.
+//
+
+import SwiftUI
+
+@main
+struct Swift_FixerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
