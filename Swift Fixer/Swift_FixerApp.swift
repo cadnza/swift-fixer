60ac58b623bc60ebac8c333b5adad8632ad3f70c
Jon Dayley <jonathandayley@gmail.com>
2023-01-26 06:30:54 -0800

    Add height and width

--- Swift Fixer/Swift_FixerApp.swift
+++ Swift Fixer/Swift_FixerApp.swift
@@ -9,9 +9,19 @@ import SwiftUI
 
 @main
 struct Swift_FixerApp: App {
+
+	let width: CGFloat = 600
+	let height: CGFloat = 300
+
     var body: some Scene {
         WindowGroup {
             ContentView()
+				.frame(
+					minWidth: width,
+					maxWidth: width,
+					minHeight: height,
+					maxHeight: height
+				)
         }
     }
 }
