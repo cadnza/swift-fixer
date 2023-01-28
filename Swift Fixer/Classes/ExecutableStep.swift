d33f2366eefcf456d010898286ce8bdebb40db19
Jon Dayley <jonathandayley@gmail.com>
2023-01-28 08:22:48 -0800

    Copy shell method to ExecutableStep class

--- Swift Fixer/Classes/ExecutableStep.swift
+++ Swift Fixer/Classes/ExecutableStep.swift
@@ -82,4 +82,36 @@ class ExecutableStep: Decodable, ObservableObject {
 		NSWorkspace.shared.open(URL(string: website)!)
 	}
 
+	func execute() -> (
+		status: Int, message: String
+	) {
+		// Open process and pipe
+		let task = Process()
+		let pipe = Pipe()
+		// Set stdin and stderr
+		task.standardOutput = pipe
+		task.standardError = pipe
+		// Set arguments
+		task.arguments = args
+		// Set launch path
+		task.launchPath =
+		Bundle.main.url(
+			forResource: exec,
+			withExtension: nil
+		)!
+			.path
+		// Initialize stdin
+		task.standardInput = nil
+		// Run task
+		do { try task.run() } catch { return (1, "Could not run") }
+		task.waitUntilExit()
+		// Get exit code
+		let code: Int = Int(task.terminationStatus)
+		// Capture output
+		let data = pipe.fileHandleForReading.readDataToEndOfFile()
+		let output = String(data: data, encoding: .utf8)!
+		// Return
+		return (code, output)
+	}
+
 }
