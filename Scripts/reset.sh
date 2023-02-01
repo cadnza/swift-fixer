b0c2969a2e91816e87866aebcadece5b263be635
Jon Dayley <jonathandayley@gmail.com>
2023-02-01 09:44:25 -0800

    Add version data

--- Scripts/reset.sh
+++ Scripts/reset.sh
@@ -10,8 +10,20 @@ imported="$dest/Imported"
 rm -rf "$imported"
 mkdir "$imported"
 
-# Copy binaries
+# Open versions file
+fVersions="$imported/versions.json"
+versions="[]"
+
+# Copy binaries and note versions
 cat "$dest/Data/commands.json" | /usr/local/bin/jq -r '.[] | .location+"/"+.title' | while read -r src
 do
 	cp "$src" "$imported"
+	version=$($src --version)
+	versions=$(
+		echo $versions | /usr/local/bin/jq -rc \
+			--arg exec $(basename $src) \
+			--arg version $(basename $version) \
+			'. += [{"exec": $exec, "version": $version}]'
+	)
+	echo $versions > $fVersions
 done
